53dd46ae29558439654897d03e08584aff1393d5
KHALID <khalidboumaghdar07@gmail.com>
2025-10-01 16:53:25 +0100

    resolve erorr

--- src/main/java/com/aplication/pharmacysystem/Repository/FondateurRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aplication.pharmacysystem.Repository;
-
-import com.aplication.pharmacysystem.model.Fondateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FondateurRepository extends JpaRepository<Fondateur, Long> {}
